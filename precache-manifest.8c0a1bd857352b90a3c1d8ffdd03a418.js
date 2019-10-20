65fd1112b71557f576f9c2e19a1391d168e30b61
yixianzi <44867804+yixianzi@users.noreply.github.com>
2019-10-20 11:13:59 +0800

    Updates

---
+++ precache-manifest.8c0a1bd857352b90a3c1d8ffdd03a418.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1280f105992dc8860d0a7eeac6f911f",
+    "url": "/test-l/index.html"
+  },
+  {
+    "revision": "94448501e5e98a74a408",
+    "url": "/test-l/static/js/2.d3a4671a.chunk.js"
+  },
+  {
+    "revision": "fa1863f0242db48e3314",
+    "url": "/test-l/static/js/main.ebb5a070.chunk.js"
+  },
+  {
+    "revision": "7522e51a38cd194f000c",
+    "url": "/test-l/static/js/runtime-main.db4d3377.js"
+  }
+]);
